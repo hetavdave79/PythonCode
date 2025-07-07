95e010752856b46b93d5396f0044c43d034eed8d
hetavdave79 <100408103+hetavdave79@users.noreply.github.com>
2025-07-07 21:35:46 +0530

    new commit

--- Basics/AllConcepts.py
+++ Basics/AllConcepts.py
@@ -6,3 +6,7 @@ names :list = ["Hetav","Stuti"]
 coord :tuple =(2,3,1.5,2.5)
 unique :set = {1,2,3,4}
 data : dict = {'Hetav':46,'Tejal':45}
+
+print(number,name,active,names,coord,unique,data)
+
+print ('HI')
